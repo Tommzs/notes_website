c00a979ce64afb22223c9c9782e69a74c041fac2
Tomas Novak <tomasnovak@Tomass-MBP.home>
2019-08-04 21:02:05 +0200

    Changed image carousel

--- js/init.js
+++ js/init.js
@@ -7,6 +7,6 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-$(document).ready(function(){
-  $('.carousel').carousel();
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true
 });
